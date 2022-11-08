520615ea1750a3615fcec11364b7b6c6db0295f7
figgge <fredrik.kvarnasen@gmail.com>
2022-11-08 12:48:39 +0100

    Added tests

---
+++ src/test/java/frkv/javafx/laborationthree/controller/PaintControllerTest.java
@@ -0,0 +1,13 @@
+package frkv.javafx.laborationthree.controller;
+
+import frkv.javafx.laborationthree.model.PaintModel;
+import javafx.scene.input.MouseEvent;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaintControllerTest {
+
+
+
+}
---
+++ src/test/java/frkv/javafx/laborationthree/model/PaintModelTest.java
@@ -0,0 +1,62 @@
+package frkv.javafx.laborationthree.model;
+
+
+import frkv.javafx.laborationthree.controller.Position;
+import frkv.javafx.laborationthree.controller.ShapeSelected;
+import javafx.scene.paint.Color;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaintModelTest {
+
+
+
+    @Test
+    void setShapeSelectedToCircle() {
+        PaintModel model = new PaintModel();
+
+        model.shapeSelectedProperty().setValue(ShapeSelected.CIRCLE);
+        model.createShape(new Position(100,100));
+
+        assertEquals(ShapeSelected.CIRCLE,model.shapeSelectedProperty().getValue(),"Selected shape should be Circle");
+    }
+
+    @Test
+    void addShapeToUndoStack() {
+        PaintModel model = new PaintModel();
+        Circle circle = new Circle(ShapeSelected.CIRCLE, new Position(0.0,0.0), model.colorProperty(), 25.0);
+
+        model.addUndoShape(circle);
+
+        var expected = 1;
+        var actual = PaintModel.undoStack.size();
+
+        assertEquals(expected,actual, "undoStack size should be 1");
+        PaintModel.undoStack.pop().run();
+    }
+
+    @Test
+    void removeShapeFromUndoStack() {
+        PaintModel model = new PaintModel();
+        Circle circle = new Circle(ShapeSelected.CIRCLE, new Position(0.0,0.0), model.colorProperty(), 25.0);
+
+        model.addUndoShape(circle);
+        model.undo();
+
+        var expected = 0;
+        var actual = PaintModel.undoStack.size();
+
+        assertEquals(expected,actual, "undoStack should be empty");
+    }
+
+
+
+
+
+
+}
